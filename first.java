9e9c0343bdd4d08f316348959b34910e45e85e64
Anmol-Pokhriyal <84918092+Anmol-Pokhriyal@users.noreply.github.com>
2022-04-12 21:31:24 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,19 @@
+package com.dit.dsa;
+
+public class first {
+
+	public static void main(String[] args) {
+//		int i,add=0,n=5;
+//for(i=0;i<=n;i++)
+//	add=add+i;
+//	System.out.println(add);
+//	
+//	
+		int n=5;
+	int sum = n*(n+1)/2;
+	System.out.println(sum);
+
+	}
+}
+
+//Questions having different time complexities
